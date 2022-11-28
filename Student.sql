bb2da66901e8e702b3ed2961800feb9c62041119
Sharuf shaik <shaiksharuf95@gmail.com>
2022-11-29 01:35:58 +0530

    Create table script updated

--- Student.sql
+++ Student.sql
@@ -1,9 +1,9 @@
 Create table "Students"(
-"Id" Integer,
-"Name" Text Not Null,
-"class" Text Not Null,
-"Mark" Integer Not Null Default 0,
+"Id"     Integer,
+"Name"   Text Not Null,
+"class"  Text Not Null,
+"Mark"   Integer Not Null Default 0,
 "Gender" Text Not Null,
-
+"DOB"     DateTime,
 Primary key ("Id" Autoincrement)
 );
