cb163ccdd4954d13f45f784678c92c825a6549d0
Sharuf shaik <shaiksharuf95@gmail.com>
2022-11-28 12:12:21 +0530

    sql project setup

---
+++ Insert-students.sql
@@ -0,0 +1,5 @@
+Insert into "Students" VALUES
+(1,"Shaik","1st","44","male"),
+(2,"Sharuf","2nd","45","male"),
+(3,"Zakeer","3rd","46","male"),
+(4,"Ash","4th","47","male")
---
+++ Student.sql
@@ -0,0 +1,9 @@
+Create table "Students"(
+"Id" Integer,
+"Name" Text Not Null,
+"class" Text Not Null,
+"Mark" Integer Not Null Default 0,
+"Gender" Text Not Null,
+
+Primary key ("Id" Autoincrement)
+);
